628582d8ced01d60e2b5033fa498dbdb50558cd0
sekthor <sysdroyed@gmail.com>
2020-09-27 13:41:11 +0200

    added buttons for easier scoring

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -11,6 +11,29 @@ public class Controller {
     // call update() when update button is pressed
     public Controller(ScoreboardView view) {
         view.getUpdate().setOnAction(event -> update(view));
+
+        view.getTry1().setOnAction(event ->
+                view.setHomeScore(add(view.getHomeScore(),5)));
+
+        view.getTry2().setOnAction(event ->
+                view.setVisitorScore(add(view.getVisitorScore(),5)));
+
+        view.getConv1().setOnAction( event ->
+                view.setHomeScore(add(view.getHomeScore(), 2)));
+
+        view.getConv2().setOnAction( event ->
+                view.setVisitorScore(add(view.getVisitorScore(), 2)));
+
+        view.getPen1().setOnAction( event ->
+                view.setHomeScore(add(view.getHomeScore(), 3)));
+
+        view.getPen2().setOnAction( event ->
+                view.setVisitorScore(add(view.getVisitorScore(), 3)));
+    }
+
+    private String add(String current, int score) {
+        Integer i = new Integer(current);
+        return Integer.toString(i + score);
     }
 
     // updates each file with current text from corresponding Text field
--- src/view/ScoreboardView.java
+++ src/view/ScoreboardView.java
@@ -10,11 +10,17 @@ import javafx.stage.Stage;
 
 public class ScoreboardView {
 
-    Button update;
-    TextField team1Name;
-    TextField team2Name;
-    TextField team1Score;
-    TextField team2Score;
+    private Button update;
+    private Button try1;
+    private Button try2;
+    private Button conv1;
+    private Button conv2;
+    private Button pen1;
+    private Button pen2;
+    private TextField team1Name;
+    private TextField team2Name;
+    private TextField team1Score;
+    private TextField team2Score;
 
     public ScoreboardView(Stage stage) {
 
@@ -29,26 +35,74 @@ public class ScoreboardView {
         this.team2Name = new TextField();
         this.team1Score = new TextField();
         this.team2Score = new TextField();
+        this.try1 = new Button("Try");
+        this.try2 = new Button("Try");
+        this.conv1 = new Button("Conversion");
+        this.conv2 = new Button("Conversion");
+        this.pen1 = new Button("Penalty");
+        this.pen2 = new Button("Penalty");
         this.update = new Button("update");
 
 
         // create an fill main pane
         GridPane main = new GridPane();
         main.add(team1,0,0);
-        main.add(team2, 2,0);
-        main.add(name, 1,1);
-        main.add(team1Name,0,2);
-        main.add(team2Name, 2,2);
-        main.add(score, 1,3);
-        main.add(team1Score,0,4);
-        main.add(team2Score, 2,4);
-        main.add(update, 1,10);
+        main.add(team2, 4,0);
+        main.add(name, 3,1);
+        main.add(score, 3,3);
+
+        main.add(team1Name,0,2,3,1);
+        main.add(team2Name, 4,2,3,1);
+        main.add(team1Score,0,4,3,1);
+        main.add(team2Score, 4,4,3,1);
+
+        main.add(try1, 0,5);
+        main.add(try2, 4,5);
+        main.add(pen1, 1,5);
+        main.add(pen2, 5,5);
+        main.add(conv1, 2,5);
+        main.add(conv2, 6,5);
+
+
+        main.add(update, 3,10);
 
 
         // set scene to stage
         Scene scene = new Scene(main);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public Button getTry1() {
+        return try1;
+    }
+
+    public Button getTry2() {
+        return try2;
+    }
+
+    public Button getConv1() {
+        return conv1;
+    }
+
+    public Button getConv2() {
+        return conv2;
+    }
+
+    public Button getPen1() {
+        return pen1;
+    }
+
+    public Button getPen2() {
+        return pen2;
+    }
+
+    public void setHomeScore(String score) {
+        this.team1Score.setText(score);
+    }
+
+    public void setVisitorScore(String score) {
+        this.team2Score.setText(score);
     }
 
     public String getHomeName() {
